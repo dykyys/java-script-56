4d3bb2cea7685bff1b3d5e3692d0d8f64e5e1b9a
dykyys <dykyy.s@gmail.com>
2022-10-05 17:32:42 +0300

    start files

---
+++ src/index.js
@@ -0,0 +1,2 @@
+'use strict';
+import 'material-icons/iconfont/material-icons.css';
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// 'https://api.unsplash.com/search';
+// LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc
+
+// /*html*/ `<li class="gallery__item">
+//     <img src="${urls.small}" alt="${alt_description}" class="gallery-img">
+// </li>`
+import 'material-icons/iconfont/material-icons.css';
+console.log('gallery');
